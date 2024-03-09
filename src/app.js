3f4ca337a60526138883c14bd73a6c485f133718
Dima Sgibnev <dimasgibnev@gmail.com>
2024-03-09 17:55:29 +0700

    work on ui

--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
-import './App.css';
+import styles from './app.module.css';
 
 export const App = () => {
-	return <></>;
+	const NUMS = [];
+	for (let i = 0; i < 10; i++) {
+		NUMS.push(`${i}`);
+	}
+	const SYMBOLS = ['+', '-', '='];
+
+	return (
+		<>
+			<div className={styles.calculator}>
+				<div className={styles.calculator__display}></div>
+				<div className={styles['calculator__button-container']}>
+					<div className={styles['button-container__numbers']}>
+						{NUMS.map((e) => (
+							<button key={e} className={styles['numbers-button']}>
+								{e}
+							</button>
+						))}
+					</div>
+					<div className={styles['button-container__symbols']}>
+						{SYMBOLS.map((e) => (
+							<button className={styles['symbols-button']}>{e}</button>
+						))}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
