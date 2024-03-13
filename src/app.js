fc6f5f2f26ebebf9299691b7e91f02bd1cc58989
Dima Sgibnev <dimasgibnev@gmail.com>
2024-03-13 18:45:15 +0700

    rebuild calc logic

--- src/app.js
+++ src/app.js
@@ -7,68 +7,60 @@ export const App = () => {
 	const [operand1, setOperand1] = useState('');
 	const [operand2, setOperand2] = useState('');
 	const [operator, setOperator] = useState('');
-	let result = 0;
-	const localOperator = localStorage.getItem('operator');
-	const promiseResult = new Promise(function (resolve, reject) {
-		if (operator === '=') {
-			resolve();
-		}
-	});
+	const [result, setResult] = useState('');
 
-	const handlePutNumber = ({ target }) => {
-		if (operator === '+' || operator === '-') {
-			if (operand2 === '0' && target.textContent === '0') {
-				setOperand2((prev) => prev + '');
-			} else if (operand2 === '0') {
-				setOperand2(target.textContent);
-			} else {
-				setOperand2((prev) => prev + target.textContent);
-			}
-		} else {
-			promiseResult.then(() => {
-				setOperand1(target.textContent);
-				setOperand2('');
-				setOperator('');
-			});
-			if (operand1 === '0' && target.textContent === '0') {
-				setOperand1((prev) => prev + '');
-			} else if (operand1 === '0') {
-				setOperand1(target.textContent);
-			} else {
-				setOperand1((prev) => prev + target.textContent);
-			}
-		}
-	};
-
-	const handlePutSymbol = ({ target }) => {
-		setOperator(target.textContent);
-		promiseResult.then(() => {
-			setOperand1(result);
-			setOperand2('');
-			setOperator(target.textContent);
-		});
-	};
-
-	if (operator === 'C') {
+	function reset() {
 		setOperand1('');
 		setOperand2('');
 		setOperator('');
-	} else if (operator === '=') {
-	} else {
-		localStorage.setItem('operator', operator);
+		setResult('');
 	}
 
-	switch (localOperator) {
-		case '+': {
-			result = Math.floor(+operand1 + +operand2);
-			break;
+	function handlePutNumber({ target }) {
+		if (operator === '+' || operator === '-') {
+			if (operand2 === '0') {
+				setOperand2((prev) => target.textContent);
+			} else {
+				setOperand2((prev) => prev + target.textContent);
+			}
+		} else if (operator === '=') {
+			if (operand1 === '0') {
+				reset();
+				setOperand1((prev) => target.textContent);
+			} else {
+				reset();
+				setOperand1((prev) => prev + target.textContent);
+			}
+		} else if (operand1 === '0') {
+			setOperand1((prev) => target.textContent);
+		} else {
+			setOperand1((prev) => prev + target.textContent);
 		}
-		case '-': {
-			result = Math.floor(+operand1 - +operand2);
-			break;
+	}
+
+	function handlePutSymbol({ target }) {
+		if (target.textContent === '=' && operator === '+') {
+			setResult(+operand1 + +operand2);
+		} else if (target.textContent === '=' && operator === '-') {
+			setResult(+operand1 - +operand2);
+		} else if (target.textContent === '=' && !operand2) {
+			setResult(operand1);
 		}
-		default:
-			break;
+
+		if (
+			operator === '=' &&
+			(target.textContent === '-' || target.textContent === '+')
+		) {
+			reset();
+			setOperand1(result);
+			setOperator(target.textContent);
+		} else {
+			setOperator(target.textContent);
+		}
+	}
+
+	if (operator === 'C') {
+		reset();
 	}
 
 	return (
